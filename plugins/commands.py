cd1dd4c424f2c8c4e5316c655c132f299022eaec
sivakarthick19 <84698947+sivakarthick19@users.noreply.github.com>
2022-01-23 14:33:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -113,11 +113,11 @@ async def start(bot, cmd):
                     ],
                     [
                         InlineKeyboardButton("sᴇᴀʀᴄʜ ʜᴇʀᴇ", switch_inline_query_current_chat=''),
-                        InlineKeyboardButton("ᴀɴʏ ʜᴇʟᴘ", url="telegram.me/STMbOTsUPPORTgROUP")
+                        InlineKeyboardButton("ᴀɴʏ ʜᴇʟᴘ", url="telegram.me/Flickrequest")
                     ],
                     [
-                        InlineKeyboardButton("ᴍʏ ᴅᴇᴠ", url="https://t.me/xxxtentacionn12"),
-                        InlineKeyboardButton("ʙᴏᴛ ᴇᴅɪᴛᴏʀ", url="https://t.me/VAMPIRE_KING_NO_1")
+                        InlineKeyboardButton("ᴍʏ ᴅᴇᴠ", url="https://t.me/UrsTrulySk"),
+                        InlineKeyboardButton("ʙᴏᴛ ᴇᴅɪᴛᴏʀ", url="https://t.me/BIRDCINEMAS")
                     ],
                     [
                         InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="about")
@@ -210,10 +210,10 @@ async def bot_info(bot, message):
     buttons = [
         [                   
             InlineKeyboardButton('💜૦ωՈ૯Ր💜', url='https://t.me/UrsTrulySk'),
-            InlineKeyboardButton('🆁🅴🅿️🅾️', url='https://t.me/'MRflicks'),
+            InlineKeyboardButton('🆁🅴🅿️🅾️', url='https://t.me/MRflicks'),
         ]
         ]
-    await message.reply(text="<b>★ 𝙼𝚈 𝙽𝙰𝙼𝙴:𝚉𝙰𝚈𝙰𝙽 𝙼𝙰𝙻𝙸𝙺\n★ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href='https://t.me/UrsTrulySk'>Sivakarthick</a>\n★ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: <code>𝙿𝚈𝚃𝙷𝙾𝙽 𝟹</code>\n★ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : <a href='https://docs.pyrogram.org/'>𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>\n𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴: <a href='https://github.com/sivakarthick19/Zayan-Malik-V2'>𝙲𝙻𝙸𝙲𝙺 𝙼𝙴 👈</a>\n★ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: <a href='https://www.mongodb.com/cloud'>𝙼𝙾𝙽𝙶𝙾 𝙳𝙱</a>\n★ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: <a href='https://heroku.com/'>𝙷𝙴𝚁𝙾𝙺𝚄</a>\n★ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: 𝚅𝟻.𝟶 [ 𝙱𝙴𝚃𝙰 ] </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply(text="<b>★ 𝙼𝚈 𝙽𝙰𝙼𝙴:MR SK\n★ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href='https://t.me/UrsTrulySk'>Sivakarthick</a>\n★ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: <code>𝙿𝚈𝚃𝙷𝙾𝙽 𝟹</code>\n★ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : <a href='https://docs.pyrogram.org/'>𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>\n𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴: <a href='https://github.com/sivakarthick19/Zayan-Malik-V2'>𝙲𝙻𝙸𝙲𝙺 𝙼𝙴 👈</a>\n★ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: <a href='https://www.mongodb.com/cloud'>𝙼𝙾𝙽𝙶𝙾 𝙳𝙱</a>\n★ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: <a href='https://heroku.com/'>𝙷𝙴𝚁𝙾𝙺𝚄</a>\n★ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: 𝚅𝟻.𝟶 [ 𝙱𝙴𝚃𝙰 ] </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
     
     
             
